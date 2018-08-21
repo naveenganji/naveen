4e4576443f33e80212359fb31b8b820320f7db16
naveenganji <naveen91.ganji@gmail.com>
2018-08-21 14:59:16 -0400

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -1,9 +1,10 @@
 class Helloworld
 {
- int n=100;
+
  public static void main (String args[])
 
 {
+   int n=100;
 System.out.println("Hello world");
 System.out.println("I edited this file");
   for(int i=1;i<=n;i++)
